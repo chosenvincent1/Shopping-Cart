542cf37914307975062585ab75557455c0bcaa81
chosen <chosendev2000@gmail.com>
2025-02-18 23:20:16 +0100

    Added filter feature to get items by category

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = ({ setShowCart })=> {
     const cartCount = useSelector(state => state.cartCounter.value);
@@ -17,16 +18,16 @@ const Navbar = ({ setShowCart })=> {
                     <a href="">Buy Now</a>
                 </div>
 
-                <div className="flex items-center gap-6 md:gap-[20px] ">
+                <div className="flex items-center ">
                     <button onClick={handleShowCart} aria-label="shopping cart" className="w-8 h-8 cursor-pointer relative">
                         <img src="../images/cart.png" alt="cart-icon" />
 
                         <div className="w-4 h-4 bg-[#f20a0a] rounded-[50%] absolute right-[-5px] top-[2px] flex items-center justify-center text-[#f3f3f3] text-xs font-bold ">{cartCount}</div>
                     </button>
 
-                    <button className="hidden md:block cursor-pointer ">
-                        <img className="w-[50px] h-10" src="../images/menu.png" alt="cart-icon" />
-                    </button>
+                    {/* <button className="hidden md:block cursor-pointer text-[20px] ">
+                        <FaBars />
+                    </button> */}
                 </div>
 
             </div>
--- src/components/ShoppingSection.jsx
+++ src/components/ShoppingSection.jsx
@@ -5,39 +5,44 @@ import { useState, useEffect } from "react";
 
 const ShoppingSection = () => {
     const [ shoppingItemData, setShoppingItemData ] = useState([]);
-    const [ itemCategory, setItemCategory ] = useState();
+    const [ itemCategory, setItemCategory ] = useState([]);
     const [ isLoading, setIsLoading ] = useState(true);
-    const [filteredItems, setFilteredItems] = useState([])
-    
-        const getShoppingItems = async () => {
-            try {
-                const response = await axios.get('https://fakestoreapi.com/products');
-                    if(response.status === 200) {
-                        setShoppingItemData(response.data);
-                        setIsLoading(false);
-                    }
-            } catch (error) {
-                console.error(error)
-            }
-        }
+    const [categories, setCategories] = useState("")
 
-        const getItemCategory = async () => {
-            try {
-                const response = await axios.get('https://fakestoreapi.com/products/categories');
+    const getItemCategory = async () => {
+        try {
+            const response = await axios.get('https://fakestoreapi.com/products/categories');
+            if(response.status === 200) {
+                setItemCategory(response.data)
+            }                
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    
+    const getShoppingItems = async () => {
+        try {
+            const response = await axios.get('https://fakestoreapi.com/products');
                 if(response.status === 200) {
-                    setItemCategory(response.data)
-                }                
-            } catch (error) {
-                console.error(error)
-            }
+                    setShoppingItemData(response.data);
+                    setIsLoading(false);
+                    
+                }
+        } catch (error) {
+            console.error(error)
         }
+    }
     
-        useEffect(()=> {
-            getShoppingItems();
-            getItemCategory();
-        }, [])
+    useEffect(()=> {
+        getShoppingItems();
+        getItemCategory();
+    }, [])
 
-
+    const filteredItems = categories === "all"
+        ? shoppingItemData 
+        : categories
+        ? shoppingItemData.filter(item => item.category === categories) 
+        : shoppingItemData;
 
     return (
         <>
@@ -45,15 +50,19 @@ const ShoppingSection = () => {
                 <p className="mt-[25%] text-center text-lg font-bold ">Loading Items...</p>
                 :
                 <section className="px-[30px] mt-[120px] md:mt-[140px] ">
-                    <select className="w-[100px] md:w-[200px] mb-6 p-2 bg-[#fff] border-1 rounded-sm ">
+                    <select
+                        value={categories}
+                        onChange={e => setCategories(e.target.value)}
+                        className="w-[100px] md:w-[200px] mb-6 p-2 bg-[#fff] border-1 rounded-sm "
+                    >
                         <option className=" " value="all">All</option>
                         {itemCategory.map((category, index) => (
-                            <option key={index} className=" " value={category}>{category}</option>
+                            <option key={index} className="" value={category}>{category}</option>
                         ))}
                     </select>
 
                     <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 md:auto-cols-fr  ">
-                        {shoppingItemData.map(item => (
+                        {filteredItems.map(item => (
                             <ShoppingItem 
                                 data={item}
                             />
