c364f2c248bf7bcbf78a1b305413d3c92b204d56
chosen <chosendev2000@gmail.com>
2025-02-18 21:25:31 +0100

    Working with fakestoreapi

--- src/components/ShoppingItem.jsx
+++ src/components/ShoppingItem.jsx
@@ -1,30 +1,36 @@
 import { useDispatch, useSelector } from "react-redux";
 import { cartIncrement, btnDisabled } from "../store/cartCounterSlice";
 
-const ShoppingItem = ({id, img, itemName, itemPrice}) => {
+
+const ShoppingItem = ({data}) => {
     const dispatch = useDispatch();
     const buttonStates = useSelector(state => state.cartCounter.buttonStates);
-    const isButtonDisabled = buttonStates[id] || false;
-
+    const isButtonDisabled = buttonStates[data.id] || false;
 
 
     return (
         <>
-            <div className="w-full shadow-sm rounded-sm " key={id}>
-                <div className="w-full h-[250px] bg-[#b4b4b4] ">
-                    <img className="w-full object-contain h-full" src={img} alt={itemName} />
+            <div className="w-full shadow-sm rounded-sm " key={data.id}>
+                <div 
+                    style={{
+                        backgroundImage: `url(${data.image})`,
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat'
+                    }}
+                    className="w-full h-[300px] ">
                 </div>
                 <div className="p-2 bg-[#e5e5e5] ">
-                    <p className="font-bold text-lg">{itemName}</p>
-                    <p className="font-bold text-[#555] text-sm ">{itemPrice}</p>
+                    <p className="text-sm font-bold mb-[2px] line-clamp-1 overflow-hidden text-ellipsis ">{data.title}</p>
+                    <p className="text-xs mb-2 font-bold text-[#555]   ">{data.category}</p>
+                    <p className="font-bold text-[#555] text-lg ">${data.price}</p>
 
                     <button 
                         onClick={() => {
                             dispatch(cartIncrement());
-                            dispatch(btnDisabled(id));
+                            dispatch(btnDisabled(data.id));
                             alert("Item added to Cart");
                         }}
-                        className={`w-full p-4 hover:bg-[#b4b4b4] mt-6 text-[#fff] font-bold rounded-sm ${
+                        className={`w-full p-4 hover:bg-[#b4b4b4] mt-2 text-[#fff] font-bold rounded-sm ${
                             isButtonDisabled ? "cursor-not-allowed bg-[#b4b4b4] " : "cursor-pointer bg-[#555] "
                         } `}
                     >{isButtonDisabled ? "Item Added" : "Add to Cart"}</button>
--- src/components/ShoppingSection.jsx
+++ src/components/ShoppingSection.jsx
@@ -1,19 +1,67 @@
 import { shoppingItems } from "../../data";
 import ShoppingItem from "./ShoppingItem";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-const ShoppingSection = ({setShowCart}) => {
+const ShoppingSection = () => {
+    const [ shoppingItemData, setShoppingItemData ] = useState([]);
+    const [ itemCategory, setItemCategory ] = useState();
+    const [ isLoading, setIsLoading ] = useState(true);
+    const [filteredItems, setFilteredItems] = useState([])
+    
+        const getShoppingItems = async () => {
+            try {
+                const response = await axios.get('https://fakestoreapi.com/products');
+                    if(response.status === 200) {
+                        setShoppingItemData(response.data);
+                        setIsLoading(false);
+                    }
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        const getItemCategory = async () => {
+            try {
+                const response = await axios.get('https://fakestoreapi.com/products/categories');
+                if(response.status === 200) {
+                    setItemCategory(response.data)
+                }                
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    
+        useEffect(()=> {
+            getShoppingItems();
+            getItemCategory();
+        }, [])
+
+
+
     return (
-        <section className="grid px-[30px] grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 md:auto-cols-fr mt-[120px] md:mt-[140px]  ">
-            {shoppingItems.map(item => (
-                <ShoppingItem 
-                    id={item.id}
-                    itemName={item.itemName}
-                    img={item.img}
-                    itemPrice={item.itemPrice}
-                    setShowCart={setShowCart}
-                />
-            ))}
-        </section>
+        <>
+            {isLoading ?
+                <p className="mt-[25%] text-center text-lg font-bold ">Loading Items...</p>
+                :
+                <section className="px-[30px] mt-[120px] md:mt-[140px] ">
+                    <select className="w-[100px] md:w-[200px] mb-6 p-2 bg-[#fff] border-1 rounded-sm ">
+                        <option className=" " value="all">All</option>
+                        {itemCategory.map((category, index) => (
+                            <option key={index} className=" " value={category}>{category}</option>
+                        ))}
+                    </select>
+
+                    <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 md:auto-cols-fr  ">
+                        {shoppingItemData.map(item => (
+                            <ShoppingItem 
+                                data={item}
+                            />
+                        ))}
+                    </div>
+                </section>
+            }
+        </>
     )
 }
 
